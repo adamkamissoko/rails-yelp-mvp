75e4d294060178a2085cc8135cb2695d0eac6897
adam kamissoko <adamkamissoko@gmail.com>
2023-10-27 07:16:06 +0400

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,38 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = {
+  name: 'Dishoom', address: '7 Boundary St, London E2 7JE',
+  category: 'japanese', phone_number: '57895000'
+}
+pizza_east = {
+  name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ',
+  category: 'italian', phone_number: '57689504'
+}
+
+mokaz = {
+  name: 'Mokaz', address: 'Moka Helvetia',
+  category: 'chinese', phone_number: '57689504'
+}
+lardoise = {
+  name: 'Lardoise', address: 'Moka Helvetia',
+  category: 'french', phone_number: '57546709'
+}
+
+baudelaire = {
+  name: 'Baudelaire', address: 'IFM Rosehill',
+  category: 'french', phone_number: '26785461'
+}
+
+[dishoom, pizza_east, mokaz, lardoise, baudelaire].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
